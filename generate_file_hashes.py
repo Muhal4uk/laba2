bcc355a99c449981b4f2e96028069df5d081f107
Muhal4uk <valerii.mykhalchuk0@gmail.com>
2025-06-13 00:23:12 +0300

    filteres

--- generate_file_hashes.py
+++ generate_file_hashes.py
@@ -1,23 +1,21 @@
 import hashlib
 
-def generate_file_hash(file_path):
-    try:
-        with open(file_path, 'rb') as file:
-            content = file.read()
-            sha256 = hashlib.sha256(content).hexdigest()
-            return sha256
-    except FileNotFoundError:
-        print(f"Файл '{file_path}' не знайдено.")
-    except IOError:
-        print(f"Помилка читання файлу '{file_path}'.")
+def generate_file_hashes(*file_paths):
+    result = {}
+    for file_path in file_paths:
+        try:
+            with open(file_path, 'rb') as file:
+                content = file.read()
+                sha256 = hashlib.sha256(content).hexdigest()
+                result[file_path] = sha256
+        except FileNotFoundError:
+            print(f"Файл '{file_path}' не знайдено.")
+        except IOError:
+            print(f"Помилка читання файлу '{file_path}'.")
+    return result
 
-    return None
-
-
+# Приклад використання
 if __name__ == "__main__":
-    file_path = "apache_logs.txt"  # 👉 тут вкажи свій файл
-    hash_result = generate_file_hash(file_path)
-    
-    if hash_result:
-        print(f"SHA-256 хеш файлу {file_path}:")
-        print(hash_result)
+    files = ["apache_logs.txt", "filtered_ips.txt"]
+    hashes = generate_file_hashes(*files)
+    print(hashes)
